aa1ebbfb6c701cce754057a54435d5f8680419a0
Lee <ylee366@gatech.edu>
2018-11-07 21:32:56 -0500

    Update

--- 02_access_multimodal_table.R
+++ 02_access_multimodal_table.R
@@ -83,17 +83,17 @@ hist(data21$pct_commute_active, freq=FALSE, breaks=seq(0, 100, by=1.25))
 hist(data21$pct_commute_other, freq=FALSE, breaks=seq(0, 100, by=1.25))
 
 data21$modality_commutes <- NA 
-data21$modality_commutes <- ifelse(data21$commute_total>0, "Other multimodals", data21$modality_commutes) 
-data21$modality_commutes <- ifelse(data21$pct_commute_car==100, "Only car user", data21$modality_commutes) 
-data21$modality_commutes <- ifelse(data21$pct_commute_car>=75 & data21$pct_commute_car<100, "Mostly car user", data21$modality_commutes) 
-data21$modality_commutes <- ifelse(data21$pct_commute_transit==100, "Only transit rider", data21$modality_commutes) 
-data21$modality_commutes <- ifelse(data21$pct_commute_transit>=50 & data21$pct_commute_transit<100, "Mostly transit rider", data21$modality_commutes) 
+data21$modality_commutes <- ifelse(data21$commute_total>0, "Other commuter", data21$modality_commutes) 
+data21$modality_commutes <- ifelse(data21$pct_commute_car==100, "Only car commuter", data21$modality_commutes) 
+data21$modality_commutes <- ifelse(data21$pct_commute_car>=75 & data21$pct_commute_car<100, "Mostly car commuter", data21$modality_commutes) 
+data21$modality_commutes <- ifelse(data21$pct_commute_transit==100, "Only transit commuter", data21$modality_commutes) 
+data21$modality_commutes <- ifelse(data21$pct_commute_transit>=50 & data21$pct_commute_transit<100, "Mostly transit commuter", data21$modality_commutes) 
 data21$modality_commutes <- ifelse(data21$pct_commute_active==100, "Only active commuter", data21$modality_commutes) 
 data21$modality_commutes <- ifelse(data21$pct_commute_active>=50 & data21$pct_commute_active<100, "Mostly active commuter", data21$modality_commutes) 
 data21$modality_commutes <- factor(data21$modality_commutes, 
-                                   levels=c("Only car user", "Mostly car user", 
-                                            "Only transit rider", "Mostly transit rider", 
-                                            "Only active commuter", "Mostly active commuter", "Other multimodals"))
+                                   levels=c("Only car commuter", "Mostly car commuter", 
+                                            "Only transit commuter", "Mostly transit commuter", 
+                                            "Only active commuter", "Mostly active commuter", "Other commuter"))
 table(data21$modality_commutes)
 sum(is.na(data21$modality_commutes)&data21$commute_total>0)
 
@@ -133,18 +133,86 @@ data21$modality_leisure <- ifelse(data21$pct_leisure_active==100, "Only active t
 data21$modality_leisure <- ifelse(data21$pct_leisure_active>=50 & data21$pct_leisure_active<100, "Mostly active traveler", data21$modality_leisure) 
 data21$modality_leisure <- factor(data21$modality_leisure, levels=c("Only car user", "Mostly car user", 
                                                                     "Only transit rider", "Mostly transit rider", 
-                                                                    "Only active commuter", "Mostly active commuter", "Other multimodals"))
+                                                                    "Only active traveler", "Mostly active traveler", "Other multimodals"))
 table(data21$modality_leisure)
-sum(is.na(data21$modality_leisure)&data21$leisure_total>0)
+sum(data21$leisure_total>0) #1913 
+sum(data21$leisure_total==0) #no leisure trips reported 
+sum(is.na(data21$modality_leisure)==TRUE) #no leisure trips reported 
+sum(is.na(data21$modality_leisure)==TRUE & data21$leisure_total>0)
 
 
-mytable <- table(data21$modality_commutes, data21$modality_leisure)
-mytable
+# mytable <- table(data21$modality_commutes, data21$modality_leisure)
+# mytable
 
-prop.table(mytable)
-prop.table(mytable, 1) # row percentage 
-prop.table(mytable, 2) # column percentage 
+# prop.table(mytable)
+# prop.table(mytable, 1) # row percentage 
+# prop.table(mytable, 2) # column percentage 
 # https://www.statmethods.net/stats/frequencies.html
 
-library(gmodels)
-CrossTable(data21$modality_commutes, data21$modality_leisure)
+# library(gmodels)
+# CrossTable(data21$modality_commutes, data21$modality_leisure)
+
+# mytable.IndMill <- table(data21[data21$Group=="IndMill", ]$modality_commutes, data21[data21$Group=="IndMill", ]$modality_leisure)
+# mytable.IndMill
+
+# round(prop.table(mytable.IndMill), 2)
+# round(prop.table(mytable.IndMill, 1), 2) # row percentage 
+# round(prop.table(mytable.IndMill, 2), 2) # column percentage 
+
+# mytable.DepMill <- table(data21[data21$Group=="DepMill", ]$modality_commutes, data21[data21$Group=="DepMill", ]$modality_leisure)
+# mytable.DepMill
+
+# round(prop.table(mytable.DepMill), 2)
+# round(prop.table(mytable.DepMill, 1), 2) # row percentage 
+# round(prop.table(mytable.DepMill, 2), 2) # column percentage 
+
+# mytable.GenXer <- table(data21[data21$Group=="GenXer", ]$modality_commutes, data21[data21$Group=="GenXer", ]$modality_leisure)
+# mytable.GenXer
+
+# round(prop.table(mytable.GenXer), 2)
+# round(prop.table(mytable.GenXer, 1), 2) # row percentage 
+# round(prop.table(mytable.GenXer, 2), 2) # column percentage 
+
+
+# modality styles for the last commute trips 
+
+data21$last_car <- ifelse(is.na(data21$F7school_pmode)==FALSE & data21$F7school_pmode>=1 & data21$F7school_pmode<=4, 1, 0)
+data21$last_transit <- ifelse(is.na(data21$F7school_pmode)==FALSE & data21$F7school_pmode>=6 & data21$F7school_pmode<=8, 1, 0)
+data21$last_active <- ifelse(is.na(data21$F7school_pmode)==FALSE & data21$F7school_pmode>=11 & data21$F7school_pmode<=13, 1, 0) 
+data21$last_other <- ifelse(is.na(data21$F7school_pmode)==FALSE & (data21$F7school_pmode==5 | data21$F7school_pmode==9 |
+                              data21$F7school_pmode==10 | data21$F7school_pmode==14), 1, 0) 
+table(data21$last_car)
+table(data21$last_transit)
+table(data21$last_active)
+table(data21$last_other)
+
+data21$last_car <- ifelse(is.na(data21$F7work_pmode)==FALSE & data21$F7work_pmode>=1 & data21$F7work_pmode<=4, 1, data21$last_car)
+data21$last_transit <- ifelse(is.na(data21$F7work_pmode)==FALSE & data21$F7work_pmode>=6 & data21$F7work_pmode<=8, 1, data21$last_transit)
+data21$last_active <- ifelse(is.na(data21$F7work_pmode)==FALSE & data21$F7work_pmode>=11 & data21$F7work_pmode<=13, 1, data21$last_active) 
+data21$last_other <- ifelse(is.na(data21$F7work_pmode)==FALSE & (data21$F7work_pmode==5 | data21$F7work_pmode==9 |
+                              data21$F7work_pmode==10 | data21$F7work_pmode==14), 1, data21$last_other) 
+table(data21$last_car+
+        data21$last_transit+
+        data21$last_active+
+        data21$last_other)
+
+data21$last_car2     <- ifelse(data21$F8school_Drivealone==1 | data21$F8school_CarpoolD==1 | data21$F8school_CarpoolP==1 | data21$F8school_Moto==1, 1, 0)
+data21$last_car2     <- ifelse(is.na(data21$last_car2)==TRUE, 0, data21$last_car2)
+data21$last_transit2 <- ifelse(data21$F8school_Bus==1 | data21$F8school_LR==1 | data21$F8school_Train==1, 1, 0)
+data21$last_transit2 <- ifelse(is.na(data21$last_transit2)==TRUE, 0, data21$last_transit2)
+data21$last_active2  <- ifelse(data21$F8school_Bike==1 | data21$F8school_Skateboard==1 | data21$F8school_Walk==1, 1, 0) 
+data21$last_active2  <- ifelse(is.na(data21$last_active2)==TRUE, 0, data21$last_active2) 
+data21$last_other2   <- ifelse(data21$F8school_Shuttle==1 | data21$F8school_Taxi==1 | data21$F8school_Uber==1 | data21$F8school_Other==1, 1, 0) 
+data21$last_other2   <- ifelse(is.na(data21$last_other2)==TRUE, 0, data21$last_other2)
+
+table(data21$last_car2)
+
+data21$last_car2     <- ifelse(data21$last_car2==0 & (data21$F8work_Drivealone==1 | data21$F8work_CarpoolD==1 | data21$F8work_CarpoolP==1 | data21$F8work_Moto==1, 1, data21$last_car2)
+data21$last_transit2 <- ifelse(data21$F8work_Bus==1 | data21$F8work_LR==1 | data21$F8work_Train==1, 1, data21$last_transit2)
+data21$last_active2  <- ifelse(data21$F8work_Bike==1 | data21$F8work_Skateboard==1 | data21$F8work_Walk==1, 1, data21$last_active2) 
+data21$last_other2   <- ifelse(data21$F8work_Shuttle==1 | data21$F8work_Taxi==1 | data21$F8work_Uber==1 | data21$F8work_Other==1, 1, data21$last_other2) 
+
+table(data21$last_car2+
+        data21$last_transit2+
+        data21$last_active2+
+        data21$last_other2)
